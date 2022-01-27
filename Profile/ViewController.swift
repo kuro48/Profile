59df9f17acaeccb84ff110fa7bbc83331829ab4b
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-01-27 17:56:46 +0900

    プロフィール応用Done!!

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -11,23 +11,53 @@ class ViewController: UIViewController {
     
     @IBOutlet var profileImageView: UIImageView!
     
+    @IBOutlet var profileLabel: UILabel!
+    
     @IBOutlet var profileCommentLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        profileImageView.isHidden = true
-        
-        profileCommentLabel.isHidden = true
     }
     
     @IBAction func tapButton1(){
+        profileImageView.image = UIImage(named: "philImage")
         
-        profileImageView.isHidden = false
+        profileLabel.text = "名前"
         
-        profileCommentLabel.isHidden = false
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+        
     }
+    
+    @IBAction func tapButton2(){
+        profileImageView.image = UIImage(named: "trackImage")
+        
+        profileLabel.text = "スポーツ"
+        
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+        
+    }
+    
+    @IBAction func tapButton3(){
+        profileImageView.image = UIImage(named: "appleImage")
+        
+        profileLabel.text = "好きな食べ物"
+        
+        profileCommentLabel.text = "リンゴが好きで、いつも持ち歩いてる"
+        
+    }
+    
+    @IBAction func tapButton4(){
+        profileImageView.image = UIImage(named: "flightImage")
+        
+        profileLabel.text = "趣味"
+        
+        profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+        
+    }
+
 
 }
 

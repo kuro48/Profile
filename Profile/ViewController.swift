9cda94dd9e085c6c0854390bfd420563c9f02d5f
RyunosukeKurokawa <kuroryu.basket@gmail.com>
2022-01-27 17:35:01 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 黒川龍之介 on 2022/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    
+    @IBOutlet var profileCommentLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        profileImageView.isHidden = true
+        
+        profileCommentLabel.isHidden = true
+    }
+    
+    @IBAction func tapButton1(){
+        
+        profileImageView.isHidden = false
+        
+        profileCommentLabel.isHidden = false
+    }
+
+}
+
